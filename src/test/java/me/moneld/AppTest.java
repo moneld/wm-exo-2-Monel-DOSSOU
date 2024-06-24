f0387242517d5fcf89751ed19eabb7011a5ee96f
Monel.D <hello@moneld.me>
2024-06-24 16:47:26 +0200

    first commit

--- src/test/java/me/moneld/AppTest.java
+++ src/test/java/me/moneld/AppTest.java
@@ -8,16 +8,16 @@ public class AppTest {
     public void testCompute() {
         App app = new App();
         assertEquals(18, app.Compute("assiette", "couscous", "coca", "moyen", "baba", "normal", "yes"));
-        assertEquals(19, app.Compute("assiette", "couscous", "coca", "moyen", "baba", "normal", "no")); 
-        assertEquals(21, app.Compute("assiette", "couscous", "coca", "grand", "baba", "special", "yes")); // 21, formule max
-        assertEquals(22, app.Compute("assiette", "couscous", "coca", "grand", "baba", "special", "no")); // 21 + 1 (pas de café)
-        assertEquals(17, app.Compute("assiette", "couscous", "coca", "petit", "baba", "normal", "yes")); // 15 + 2 (boisson) + 2 (dessert) - 2 (1 café offert, 1 café payé)
-        assertEquals(18, app.Compute("assiette", "couscous", "coca", "petit", "baba", "normal", "no")); // 15 + 2 (boisson) + 2 (dessert) + 1 (pas de café)
-        assertEquals(13, app.Compute("sandwich", "sandwich", "coca", "moyen", "baba", "normal", "yes")); // 13, formule standard
-        assertEquals(14, app.Compute("sandwich", "sandwich", "coca", "moyen", "baba", "normal", "no")); // 13 + 1 (pas de café)
-        assertEquals(16, app.Compute("sandwich", "sandwich", "coca", "grand", "baba", "special", "yes")); // 16, formule max
-        assertEquals(17, app.Compute("sandwich", "sandwich", "coca", "grand", "baba", "special", "no")); // 16 + 1 (pas de café)
-        assertEquals(14, app.Compute("sandwich", "sandwich", "coca", "petit", "baba", "normal", "yes")); // 10 + 2 (boisson) + 2 (dessert) + 1 (café)
-        assertEquals(15, app.Compute("sandwich", "sandwich", "coca", "petit", "baba", "normal", "no")); // 10 + 2 (boisson) + 2 (dessert) + 1 (pas de café)
+        assertEquals(19, app.Compute("assiette", "couscous", "coca", "moyen", "baba", "normal", "no"));
+        assertEquals(21, app.Compute("assiette", "couscous", "coca", "grand", "baba", "special", "yes"));
+        assertEquals(22, app.Compute("assiette", "couscous", "coca", "grand", "baba", "special", "no"));
+        assertEquals(17, app.Compute("assiette", "couscous", "coca", "petit", "baba", "normal", "yes"));
+        assertEquals(18, app.Compute("assiette", "couscous", "coca", "petit", "baba", "normal", "no"));
+        assertEquals(13, app.Compute("sandwich", "sandwich", "coca", "moyen", "baba", "normal", "yes"));
+        assertEquals(14, app.Compute("sandwich", "sandwich", "coca", "moyen", "baba", "normal", "no"));
+        assertEquals(16, app.Compute("sandwich", "sandwich", "coca", "grand", "baba", "special", "yes"));
+        assertEquals(17, app.Compute("sandwich", "sandwich", "coca", "grand", "baba", "special", "no"));
+        assertEquals(14, app.Compute("sandwich", "sandwich", "coca", "petit", "baba", "normal", "yes"));
+        assertEquals(15, app.Compute("sandwich", "sandwich", "coca", "petit", "baba", "normal", "no"));
     }
 }
